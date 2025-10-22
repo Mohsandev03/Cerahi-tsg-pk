8a7e40584c37667e51c5f0b0b26a6e7972e92622
Mohsandev03 <mohsan.k@itkumail.com>
2025-10-22 17:29:07 +0500

    categories

---
+++ categories/shooting-gloves/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+import TopNavOne from "@/components/Header/TopNav/TopNavOne";
+import MenuOne from "@/components/Header/Menu/MenuOne";
+import CategoryBreadCrumb1 from "@/components/Shop/ShootingGlovesBreadcrumb";
+import productData from "@/data/Product.json";
+import Footer from "@/components/Footer/Footer";
+import ActiveWearBanner from "@/components/Slider/ShootingGlovesSlider";
+import Testimonial from "@/components/Home1/Testimonial";
+import testimonialData from "@/data/Testimonial.json";
+import Banner3 from "@/components/Home1/Banner3";
+import CapsPopularCategories from "@/components/Home1/capspapularcategories";
+import ShootingGlovesbanner from "@/components/Banners/ShootingGlovesbanner";
+import Brand from "@/components/Home1/Brand";
+import Instagram from "@/components/Home1/Instagram";
+
+export default function DefaultGrid() {
+  const searchParams = useSearchParams();
+  let type = searchParams.get("type");
+  let gender = searchParams.get("gender");
+  let category = searchParams.get("category");
+
+  return (
+    <>
+      <TopNavOne
+        props="style-one  bg-black"
+        slogan="New customers save 10% with the code GET10"
+      />
+      <div id="header" className="relative w-full">
+        <MenuOne props="bg-transparent" />
+      </div>
+      <ActiveWearBanner />
+      <CategoryBreadCrumb1
+        data={productData.map((item: any) => ({
+          tabs: [],
+          ...item,
+        }))}
+        productPerPage={9}
+        dataType={type}
+        gender={gender}
+        category={category}
+      />
+      <ShootingGlovesbanner />
+
+      <CapsPopularCategories />
+
+      <Banner3 />
+      <Testimonial data={testimonialData} limit={6} />
+      <Instagram />
+      <Brand />
+
+ 
+      <Footer />
+    </>
+  );
+}
